c987f085c477fe352330cf643ca7cd6f600b855d
莫忘输赢 <46436509+mowangshuying@users.noreply.github.com>
2023-04-26 21:12:12 +0800

    update

--- fluent-icon/main.cpp
+++ fluent-icon/main.cpp
@@ -17,13 +17,32 @@ int main(int argc, char** argv)
 	QApplication::setFont(fluentFont);
 
 	QWidget w;
-	QPushButton btn(&w);
 
-	QChar c = QChar(0xf11b);
-	//btn.setFont(fluentFont);
-	btn.setText(QString(QChar(0xe702)));
+	//QPushButton btn(&w);
+	//QChar c = QChar(0xf11b);
+	////btn.setFont(fluentFont);
+	//btn.setText(QString(QChar(0xe702)));
 
-	//btn.setText("Btn");
+
+	QPixmap pix(30, 30);
+	pix.fill(Qt::transparent);
+	//设置画笔
+	QPainter painter;
+	painter.begin(&pix);
+	//消除按钮按下的效果
+	painter.setRenderHints(QPainter::Antialiasing | QPainter::TextAntialiasing);
+	//设置画笔颜色
+	painter.setPen(QColor("blue"));
+	//设置刷子颜色
+	painter.setBrush(QColor("blue"));
+	//设置图形大小，这个大小主要取决与icon大小，icon设置的比较小，这个设置再大也没用
+	fluentFont.setPointSize(10);
+	painter.setFont(fluentFont);
+	//画图形字体
+	painter.drawText(pix.rect(), Qt::AlignCenter, QChar(0xe712));
+	painter.end();
+
+	pix.save("tmp.png");
 
 	w.show();
 
